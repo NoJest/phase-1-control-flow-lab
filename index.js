3f936c0cbf67861040f59243c3a7f09815079b11
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-10-08 13:58:29 -0400

    tricky but did it

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+   return "This one is on me!"};
+   if ( someNumber <=2000) {
+    return "That will be twenty bucks."
+   }
+   if (someNumber < 2500) {
+    return 'I will gladly take your thirty bucks.'
+   }
+   else if (someNumber > 2500) {
+    return 'No can do.'
+   }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  const cityMessage = someCity === 'NYC' ? "Ok, sounds good." : 'No go.'
+return cityMessage
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+if (tip === 'generous') { 
+  return'Thank you so much.'
+} else if (tip==='not as generous'){
+  return'Thank you.'
+} else {
+return 'Bye.'
+}
 }
